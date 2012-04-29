e2db5548510bcdfe77e695da3cb6f2f7daef5cf0
Skylar Saveland <skylar.saveland@gmail.com>
2012-04-28 22:07:57 -0400

    nothing

--- tutor/song3.py
+++ tutor/song3.py
@@ -12,23 +12,25 @@ from tutor.complib import bass_f, kit_f
 
 notes = Adder(RandomArp())
 notes.reset([60, 60, 62, 60, 60, 60, 60, 61, 48, 50, 60, 60, 60, 60, 60, 71, 60,
-             60,60,60,60,60,60,50])
+             60, 60, 60, 60, 60, 60, 50])
 notes.amount = -18
 
-velocity = Adder(OrderedArp([120,80,89,83,120,120,80,79]))
+velocity = Adder(OrderedArp([120, 80, 89, 83, 120, 120, 80, 79]))
 
 bass = bass_f()
 kit = kit_f()
 layer = Layer([bass, kit])
 
-player = Player(layer, notes, velocity, stop=lambda: random.randint(12,60),
+player = Player(layer, notes, velocity, stop=lambda: random.randint(12, 60),
                 interval=0.0625)
 player.startPlaying()
 
 
 # Now we see how we can set up a simple "process" to change the key periodically
 
-keys = cycle([-18] * 8 + [-18,-37,-18,-37] * 4 + [-18,-26,-18,-26,-37,-26,-18])
+keys = cycle([-18] * 8 + [-18, -37, -18, -37] * 4 + [-18, -26, -18, -26, -37, -26, -18])
+
+
 def change_key():
     notes.amount = keys.next()
 change_key_event = clock.schedule(change_key)
